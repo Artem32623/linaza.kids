9a65c186b29c327f4971d510c637ffa66363e37b
Luffy <knyshevichartem@gmail.com>
2023-07-04 12:31:34 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$('.main__offer__parent').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: '<button type="button" class="slick-next main__offer__buttons">></button>',
+    prevArrow: '<button type="button" class="slick-prev main__offer__buttons"><</button>',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+
+    ]
+});
+
+$('.main__wrapper').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+});
